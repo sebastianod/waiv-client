5d4320f0605e960357b24ca99673f121b5ecf04b
sebastian <sebastianochoad@gmail.com>
2023-04-08 11:42:15 -0500

    Changed <Output/> so that it shows whether an image is loading or not based on the WaveContext isLoading state set by useSendReceiveData.

--- src/components/output/output.component.jsx
+++ src/components/output/output.component.jsx
@@ -1,14 +1,18 @@
+import { useContext } from "react";
+import { WaveContext } from "../../context/wave.context";
 import "./output.styles.scss";
 
 //show default image and then the generated images
 const Output = (props) => {
   const { svgText } = props; //destructuring
+  const { isLoading } = useContext(WaveContext);
 
   //relative path cannot be used outside the src folder, thus:
   //../../public/default.svg, doesn't work
   // {process.env.PUBLIC_URL + "/default.svg"}, works
   return (
     <div className="output-container">
+      {isLoading && <p>Loading...</p>}
       {!svgText ? (
         <img
           src={process.env.PUBLIC_URL + "/default.svg"}
